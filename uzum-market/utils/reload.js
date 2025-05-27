fc993c39922675b1cd202fcb13f0f44a1ca8c359
akbarasadov <akbarasadov4170@gmail.com>
2025-05-27 07:59:13 +0500

    Delete uzum-market directory

--- uzum-market/utils/reload.js
+++
@@ -1,9 +0,0 @@
-export function reload(arr, component, place) {
-
-    place.innerHTML = ""
-
-    for (const item of arr) {
-        const elem = component(item)
-        place.append(elem)
-    }
-}
