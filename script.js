897a7a3d04b4e25cf62ea53450eaabbd9b5d1a66
akbarasadov <akbarasadov4170@gmail.com>
2025-05-29 11:07:32 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,57 +1,32 @@
-import { categories } from "./components/categories.js";
-import { reload } from "./utils/reload.js";
-import { add } from "./components/product.js";
+import { Products } from "./components/product"
+import { Toproduct } from "./components/to-product"
 
-let res = await fetch("https://6832b59ec3f2222a8cb333b6.mockapi.io/api/v1/products");
-let products = await res.json();
 
-let productPlace = document.querySelector(".grid");
-reload(products, add, productPlace);
+let grid = document.querySelector(".grid")
 
-let res1 = await fetch("https://6832b59ec3f2222a8cb333b6.mockapi.io/api/v1/categories");
-let categoriesData = await res1.json();
+grid.append(Toproduct())
 
-let categoryPlace = document.querySelector(".categories");
-categoryPlace.innerHTML = "";
+let plus = document.querySelector(".plus")
+let minus = document.querySelector(".minus")
+let pices_text = document.querySelector(".text")
 
-for (let item of categoriesData) {
-    const elem = categories(item, products, productPlace);
-    categoryPlace.appendChild(elem);
+
+
+minus.onclick = () => {
+    if (pices_text.textContent < 2) {
+        pices_text.textContent = 1
+    } else {
+        pices_text.textContent--
+    }
 }
 
-const track = document.querySelector('.slider-track');
-const slides = document.querySelectorAll('.slide');
-const dotsContainer = document.querySelector('.slider-dots');
-const leftBtn = document.querySelector('.arrow.left');
-const rightBtn = document.querySelector('.arrow.right');
-
-let current = 0;
-
-
-slides.forEach((_, index) => {
-    const dot = document.createElement('span');
-    dot.addEventListener('click', () => goToSlide(index));
-    dotsContainer.appendChild(dot);
-});
-
-const dots = dotsContainer.querySelectorAll('span');
-
-function goToSlide(index) {
-    if (index < 0) index = slides.length - 1;
-    if (index >= slides.length) index = 0;
-
-    track.style.transform = `translateX(-${index * 100}%)`;
-    dots.forEach(dot => dot.classList.remove('active'));
-    dots[index].classList.add('active');
-    current = index;
+plus.onclick = () => {
+    pices_text.textContent++
 }
 
-let interval = setInterval(() => {
-    goToSlide(current + 1)
-}, 2500);
 
-leftBtn.addEventListener('click', () => goToSlide(current - 1));
-rightBtn.addEventListener('click', () => goToSlide(current + 1));
+// let res = await fetch("https://6832b59ec3f2222a8cb333b6.mockapi.io/api/v1/products");
+// let products = await res.json();
 
-
-goToSlide(0);
+// let productPlace = document.querySelector(".products_place");
+// reload(products, Products, productPlace);
